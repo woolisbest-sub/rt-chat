eb4467e75ed8843a03a8853b2f01d250c3720073
woolisbest-sub <woolisbest@outlook.com>
2026-02-11 17:25:40 +0900

    Update server to listen on all network interfaces

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http=require("http");
 const WebSocket=require("ws");
+
 const PORT=process.env.PORT||3000;
 
 const page=`<!DOCTYPE html><html><body>
@@ -10,11 +11,9 @@ const page=`<!DOCTYPE html><html><body>
 <script>
 const w=new WebSocket((location.protocol==="https:"?"wss://":"ws://")+location.host);
 w.onmessage=e=>l.textContent+=e.data+"\\n";
-function s(){
-w.send(JSON.stringify({name:n.value,msg:m.value}));
-m.value="";
-}
-</script></body></html>`;
+function s(){w.send(JSON.stringify({name:n.value,msg:m.value}));m.value="";}
+</script>
+</body></html>`;
 
 const server=http.createServer((req,res)=>{
 res.writeHead(200,{"Content-Type":"text/html"});
@@ -31,4 +30,4 @@ if(c.readyState===1)c.send(msg.toString());
 });
 });
 
-server.listen(PORT);
+server.listen(PORT,"0.0.0.0");
